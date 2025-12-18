c5d2617764a565e61065f9264b1504c00f684d30
Alonso <alonso.de@alumni.immune.institute>
2025-12-18 14:56:45 +0100

    Añadido seleccion de tipo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,17 @@ function App() {
 
   return (
     <>
-      <h1>Título de la web</h1>
+      <label for="inputTipo">Tipo: </label>
+      <select name="" id="inputTipo">
+        <option value="">Nada</option>
+        <option value="SUV">SUV</option>
+        <option value="Berlina">Berlina</option>
+        <option value="Tourin">Touring</option>
+        <option value="Gran Coupé">Gran Coupé</option>
+        <option value="Compacto">Compacto</option>
+        <option value="Monovolumen">Monovolumen</option>
+        <option value="Cabrio">Cabrio</option>
+      </select>
     </>
   )
 }
